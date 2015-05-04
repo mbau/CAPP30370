87ba0bb7ff7f12d765da81e9e0302c3263e36cba
Michael Bau <mbau@nd.edu>
2015-05-04 00:52:14 -0400

    added animation css

--- projects/final-project/javascript/dining.js
+++ projects/final-project/javascript/dining.js
@@ -2,19 +2,6 @@ $(document).ready( function() {
 	var viewportWidth = $(window).width();
 	console.log("Width: " + viewportWidth);
 
-	var cycle = $(".cycle");
-	var cycleIndex = -1;
-
-	function showNextCycle() {
-		++cycleIndex;
-		cycle.eq(cycleIndex % cycle.length)
-		.fadeIn(1000)
-		.delay(3000)
-		.fadeOut(1000, showNextCycle);
-	}
-
-	showNextCycle();
-
 });
 
 $(window).resize(function() {
